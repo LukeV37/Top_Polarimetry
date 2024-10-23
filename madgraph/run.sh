748faab71788bb7fb7692d17b7960720b9d450d8
lvaughan <luke.vaughan@okstate.edu>
2024-10-22 23:19:40 -0500

    update path

--- madgraph/run.sh
+++ madgraph/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run mg5_aMC binary on the process card
-../modules/mg5amcnlo-v3.5.5/bin/mg5_aMC cards/proc_card_mg5.dat
+../submodules/mg5amcnlo-v3.5.5/bin/mg5_aMC cards/proc_card_mg5.dat
 
 # Copy the cuts.f card to the SubProcesses folder
 cp ./include/cuts.f ./pp_tt_semi_full/SubProcesses/
@@ -13,4 +13,4 @@ cp ./cards/run_card.dat ./pp_tt_semi_full/Cards/
 ./pp_tt_semi_full/bin/generate_events
 
 # Clean up workspace (generated automatically by madgraph binary)
-rm py.py
+rm -f py.py
