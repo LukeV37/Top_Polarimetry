ff5ee58835874cd87133e0a9863bf4dd98a5eb89
lvaughan <luke.vaughan@okstate.edu>
2024-11-12 23:59:43 -0600

    pythia configure update

--- submodules/install_scripts/install_pythia.sh
+++ submodules/install_scripts/install_pythia.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 cd ../pythia-v8.312
-./configure --with-hepmc2=../hepmc-v2.06.11/hepmc-install --prefix=$PWD --with-gzip
+./configure --with-root --with-hepmc2=../hepmc-v2.06.11/hepmc-install --prefix=$PWD --with-gzip
 make -j4
