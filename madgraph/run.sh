37cc277f306f2d8bb49ee8116d58a26c269a2e72
lvaughan <luke.vaughan@okstate.edu>
2024-11-13 00:03:04 -0600

    madgraph production version

--- madgraph/run.sh
+++ madgraph/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Run mg5_aMC binary on the process card
-../submodules/mg5amcnlo-v3.5.5/bin/mg5_aMC cards/proc_card_mg5.dat
+MG5_aMC_v3_5_6/bin/mg5_aMC cards/proc_card_mg5.dat
 
 # Copy the cuts.f card to the SubProcesses folder
 cp ./include/cuts.f ./pp_tt_semi_full/SubProcesses/
 
 # Copy the run card to the Cards folder
 cp ./cards/run_card.dat ./pp_tt_semi_full/Cards/
+cp ./cards/pythia8_card.dat ./pp_tt_semi_full/Cards/
+#cp ./cards/delphes_card_HLLHC.tcl ./pp_tt_semi_full/Cards/
 
 # Generate the Events!
 ./pp_tt_semi_full/bin/generate_events
