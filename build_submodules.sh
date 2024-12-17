f109b17f74192cac253dca06ee6e01fef9c31dd7
lvaughan <luke.vaughan@okstate.edu>
2024-12-17 12:44:52 -0600

    clean build

--- build_submodules.sh
+++ build_submodules.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd submodules/install_scripts
-#./install_hepmc.sh
-#./install_pythia.sh
+./clean_submodules.sh
+./install_hepmc.sh
+./install_fastjet.sh
+./install_pythia.sh
 ./install_delphes.sh
