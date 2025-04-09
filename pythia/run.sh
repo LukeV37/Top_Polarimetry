5474b0c795614e0d3c91c5059ea881af0685d042
lvaughan <luke.vaughan@okstate.edu>
2025-04-09 15:43:56 -0500

    implement batching for max_cpu_cores

--- pythia/run.sh
+++ pythia/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-    echo "Must enter 2 agruments"
+if [ -z "$3" ]; then
+    echo "Must enter 3 agruments"
     echo -e "\t1: Dataset Tag (from MadGraph)"
     echo -e "\t2: Number of runs (from MadGraph)"
+    echo -e "\t3: Number of batches (based on cpu cores)"
     exit 1
 fi
 
 tag=$1
 runs=$2
+max_cpu_cores=$3
 
 mkdir "WS_${tag}"
 mkdir "WS_${tag}/logs"
@@ -18,9 +20,19 @@ make generate_dataset
 
 echo "Please be patient while Pythia performs hadronization..."
 
+job=0
+batch=1
 for (( i=0 ; i<$runs ; i++ ));
 do
+    echo -e "\t\tSubmitting job to shower run $i"
     ./run_dataset $tag $i > "../WS_${tag}/logs/dataset_${tag}_${i}.log" 2>&1 &
+    job=$((job+1))
+    if [ $job == $max_cpu_cores ]; then
+        echo -e "\tStopping jobs submissions! Please wait for batch $batch to finish..."
+        wait
+        job=0
+        batch=$((batch+1))
+    fi
 done
 wait
 make clean
