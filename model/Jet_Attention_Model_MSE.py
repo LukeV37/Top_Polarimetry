d6bdeb735f9306e88da71998a311c1142027b581
lvaughan <luke.vaughan@okstate.edu>
2025-04-21 15:26:45 -0500

    import model from external file

--- model/Jet_Attention_Model_MSE.py
+++ model/Jet_Attention_Model_MSE.py
@@ -11,6 +11,8 @@ import torch.optim as optim
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score, roc_auc_score
 import sys
 
+from model import Model
+
 tag = str(sys.argv[1])
 Epochs = int(sys.argv[2])
 Step = int(sys.argv[3])
@@ -20,12 +22,12 @@ analysis_type = str(sys.argv[6])
 
 assert analysis_type=="bottom" or analysis_type=="down" or analysis_type=="top"
 
+if analysis_type=="top":
+    feats = ['top_px','top_py','top_pz','top_E']
 if analysis_type=="bottom":
     feats = ['bottom_px','bottom_py','bottom_pz']
 if analysis_type=="down":
     feats = ['down_px','down_py','down_pz']
-if analysis_type=="top":
-    feats = ['top_px','top_py','top_pz','top_E']
 num_feats = len(feats)
 
 with open(in_dir+"/data_batched_combined_MSE_"+tag+".pkl","rb") as f:
@@ -57,91 +59,6 @@ y_test_jet_trk = data_dict["jet_trk_labels_batch"][test_split:]
 print("Training Batches: ", len(y_train))
 print("Validation Batches: ", len(y_val))
 print("Testing Batches: ", len(y_test))
-
-class Encoder(nn.Module):
-    def __init__(self, embed_dim, num_heads):
-        super(Encoder, self).__init__()
-        self.pre_norm_Q = nn.LayerNorm(embed_dim)
-        self.pre_norm_K = nn.LayerNorm(embed_dim)
-        self.pre_norm_V = nn.LayerNorm(embed_dim)
-        self.attention = nn.MultiheadAttention(embed_dim,num_heads=num_heads,batch_first=True, dropout=0.25)
-        self.post_norm = nn.LayerNorm(embed_dim)
-        self.out = nn.Linear(embed_dim,embed_dim)
-    def forward(self, Query, Key, Value):
-        Query = self.pre_norm_Q(Query)
-        Key = self.pre_norm_K(Key)
-        Value = self.pre_norm_V(Value)
-        context, weights = self.attention(Query, Key, Value)
-        context = self.post_norm(context)
-        latent = Query + context
-        tmp = F.gelu(self.out(latent))
-        latent = latent + tmp
-        return latent, weights
-
-class Stack(nn.Module):
-    def __init__(self, embed_dim, num_heads):
-        super(Stack, self).__init__()
-        self.jet_trk_encoder = Encoder(embed_dim, num_heads)
-        self.trk_encoder = Encoder(embed_dim, num_heads)
-        self.jet_trk_cross_encoder = Encoder(embed_dim, num_heads)
-        self.trk_cross_encoder = Encoder(embed_dim, num_heads)
-    def forward(self, jet_embedding, jet_trk_embedding, trk_embedding):
-        # Jet Track Attention
-        jet_trk_embedding, jet_trk_weights = self.jet_trk_encoder(jet_trk_embedding, jet_trk_embedding, jet_trk_embedding)
-        # Cross Attention (Local)
-        jet_embedding, cross_weights = self.jet_trk_cross_encoder(jet_embedding, jet_trk_embedding, jet_trk_embedding)
-        # Track Attention
-        trk_embedding, trk_weights = self.trk_encoder(trk_embedding, trk_embedding, trk_embedding)
-        # Cross Attention (Global)
-        jet_embedding, cross_weights = self.trk_cross_encoder(jet_embedding, trk_embedding, trk_embedding)
-        return jet_embedding, jet_trk_embedding, trk_embedding
-
-class Model(nn.Module):  
-    def __init__(self):
-        super(Model, self).__init__()   
-        
-        self.embed_dim = 32
-        self.num_heads = 4
-        self.num_jet_feats = 4
-        self.num_trk_feats = 6
-        
-        # Initiliazer
-        self.jet_initializer = nn.Linear(self.num_jet_feats, self.embed_dim)
-        self.jet_trk_initializer = nn.Linear(self.num_trk_feats, self.embed_dim)
-        self.trk_initializer = nn.Linear(self.num_trk_feats, self.embed_dim)
-           
-        # Transformer Stack
-        self.stack1 = Stack(self.embed_dim, self.num_heads)
-        self.stack2 = Stack(self.embed_dim, self.num_heads)
-        self.stack3 = Stack(self.embed_dim, self.num_heads)
-
-        # Regression Task
-        self.regression = nn.Linear(self.embed_dim, num_feats)
-        # Classification Task
-        self.jet_trk_classification = nn.Linear(self.embed_dim, 3)
-
-    def forward(self, jets, jet_trks, trks):
-        
-        # Feature preprocessing layers
-        jet_embedding = F.gelu(self.jet_initializer(jets))
-        jet_trk_embedding = F.gelu(self.jet_trk_initializer(jet_trks))
-        trk_embedding = F.gelu(self.trk_initializer(trks))
-        jet_embedding = torch.unsqueeze(jet_embedding, 1)
-        
-        # Transformer Stack
-        jet_embedding, jet_trk_embedding, trk_embedding = self.stack1(jet_embedding,jet_trk_embedding,trk_embedding)
-        jet_embedding, jet_trk_embedding, trk_embedding = self.stack2(jet_embedding,jet_trk_embedding,trk_embedding)
-        jet_embedding, jet_trk_embedding, trk_embedding = self.stack3(jet_embedding,jet_trk_embedding,trk_embedding)
-    
-        # Get output
-        jet_embedding = torch.squeeze(jet_embedding,1)
-        if analysis_type=="bottom" or analysis_type=="down":
-            output = F.tanh(self.regression(jet_embedding))
-        if analysis_type=="top":
-            output = self.regression(jet_embedding)
-        jet_trk_classification = self.jet_trk_classification(jet_trk_embedding)
-        
-        return output, jet_trk_classification
 
 ### Define Training Loop
 def train(X_train_jet, X_train_jet_trk, X_train_trk, y_train, y_train_jet_trk,
@@ -220,7 +137,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 print()
 
-model = Model()
+model = Model(64,4,num_feats,analysis_type)
 model.to(device)
 print("Trainable Parameters :", sum(p.numel() for p in model.parameters() if p.requires_grad))
 
