9a76c7faca026c13f55e8bbc41b6bc1f7fcf9081
lvaughan <luke.vaughan@okstate.edu>
2025-09-22 20:18:51 -0500

    working pipeline

--- run_job.sh
+++ run_job.sh
@@ -50,9 +50,8 @@ if [ "$bypass_preprocessing" = false ]; then
   for (( i=0 ; i<$num_runs ; i++ ));
   do
     echo -e "\t\tSubmitting job to preprocess run $i"
-    mkdir -p "${dir_preprocessing}/run_$i"
     mkdir -p "${dir_datasets}/run_$i"
-    python -u Preprocessing.py $tag $i $dir_preprocessing $dir_datasets $analysis_type > "${dir_preprocessing}/run_$i/preprocessing.log" &
+    python -u DataLoader_Parallel.py $tag $i $dir_datasets > "${dir_datasets}/run_$i/preprocessing.log" &
     job=$((job+1))
     if [ $job == $max_cpu_cores ]; then
       echo -e "\tStopping jobs submissions! Please wait for batch $batch to finish..."
@@ -63,37 +62,7 @@ if [ "$bypass_preprocessing" = false ]; then
   done
   wait
 
-  cd $WORKING_DIR
-  end=`date +%s`
-  runtime=$((end-start))
-  echo -e "\tPreprocessing Done!"
-  echo -e "\tTime (sec): $runtime"
-fi
-
-# Run batching to pad dataset
-if [ "$bypass_batch" = false ]; then
-  echo "Please be patient for Batching..."
-  start=`date +%s`
-  cd model
-
-  job=0
-  batch=1
-  for (( i=0 ; i<$num_runs ; i++ ));
-  do
-    echo -e "\t\tSubmitting job to pad and batch run $i"
-    mkdir -p "${dir_datasets}/logs"
-    python -u Batch_MSE.py $tag $i $dir_datasets > "${dir_datasets}/logs/batch.log" &
-    job=$((job+1))
-    if [ $job == $max_cpu_cores ]; then
-      echo -e "\tStopping jobs submissions! Please wait for batch $batch to finish..."
-      wait
-      job=0
-      batch=$((batch+1))
-    fi
-  done
-  wait
-
-  python -u Combine_Batches.py $tag $num_runs $dir_datasets
+  python -u Combine_DataSets.py $tag $num_runs $dir_datasets
   cd $WORKING_DIR
   end=`date +%s`
   runtime=$((end-start))
@@ -107,7 +76,7 @@ if [ "$bypass_train" = false ]; then
   start=`date +%s`
   cd model
   mkdir -p $dir_training
-  python -u Jet_Attention_Model_MSE.py $tag $epochs $step $dir_datasets $dir_training $analysis_type | tee "${dir_training}/training.log"
+  python -u New_Training.py $tag $epochs $dir_datasets $dir_training | tee "${dir_training}/training.log"
   cd $WORKING_DIR
   end=`date +%s`
   runtime=$((end-start))
