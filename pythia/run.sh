95d0aaf8ae11cc6b4b5dc15a9feccf299c73f0f6
LukeV37 <luke.vaughan@okstate.edu>
2026-06-10 14:27:50 -0500

    rm deprecated features

--- pythia/run.sh
+++ pythia/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$6" ]; then
-    echo "Must enter 4 agruments"
+    echo "Must enter 6 agruments"
     echo -e "\t1: Dataset Tag (from MadGraph)"
     echo -e "\t2: Dataset Tag (for Pythia)"
     echo -e "\t3: Number of runs (from MadGraph)"
@@ -23,7 +23,6 @@ mkdir -p "WS_${PY_tag}/logs"
 
 cd src
 make generate_dataset
-make selections
 
 echo "Please be patient while Pythia performs hadronization..."
 
@@ -32,7 +31,6 @@ batch=1
 for (( i=0 ; i<$runs ; i++ ));
 do
     echo -e "\t\tSubmitting job to shower run $i"
-    #(./run_dataset $tag $i; ./run_selections $tag $i) > "../WS_${tag}/logs/dataset_${tag}_${i}.log" 2>&1 &
     ./run_dataset $MG_tag $PY_tag $i $R $minpT > "../WS_${PY_tag}/logs/dataset_${PY_tag}_${i}.log" 2>&1 &
     job=$((job+1))
     if [ $job == $max_cpu_cores ]; then
