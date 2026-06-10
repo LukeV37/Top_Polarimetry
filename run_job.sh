21017bb6e6d162628d7d12300a4790d91931060f
LukeV37 <luke.vaughan@okstate.edu>
2026-06-10 15:11:22 -0500

    feat: configurable top quark pT cut

--- run_job.sh
+++ run_job.sh
@@ -17,7 +17,7 @@ source setup.sh
 if [ "$bypass_madgraph" = false ]; then
   start=`date +%s`
   cd madgraph
-  ./run.sh $MG_tag $process $generation $num_runs $num_events_per_run $max_cpu_cores $seed
+  ./run.sh $MG_tag $process $generation $num_runs $num_events_per_run $max_cpu_cores $seed $top_pT_cut
   cd $WORKING_DIR
   end=`date +%s`
   runtime=$((end-start))
