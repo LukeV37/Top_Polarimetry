bcca011ec4769f076e4b28d3c58f693f98fcecdc
LukeV37 <luke.vaughan@okstate.edu>
2026-06-10 15:12:28 -0500

    rm unnecessary dir

--- configs/check_configs.sh
+++
@@ -1,5 +0,0 @@
-for file in configs/*; do
-    echo "File: $file"
-    sed -n '14p;24p;25p' "$file"
-    echo ""
-done
--- configs/run_jobs.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-for file in configs/configs/*; do
-    filename=$(basename "$file" .config)
-    echo "File: $file"
-    ./run_job.sh $file > configs/logs/$filename.log
-    echo ""
-done
-cd configs
